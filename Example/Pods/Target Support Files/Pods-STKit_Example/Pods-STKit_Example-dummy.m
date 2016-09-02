154f9c071c808960f47651e8af1b21de9fded606
bin <bin@bindeMacBook-Pro.local>
2016-09-02 15:17:54 +0800

    init proj

---
+++ Example/Pods/Target Support Files/Pods-STKit_Example/Pods-STKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_STKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_STKit_Example
+@end
---
+++ Example/Pods/Target Support Files/STKit/STKit-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double STKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char STKitVersionString[];
+
